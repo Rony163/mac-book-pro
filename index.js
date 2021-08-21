ef86e2816eab36d8023703cf9c30d628778fa4db
Rony163 <minhajulislamrony43@gmail.com>
2021-08-21 12:06:07 +0600

    function added

--- index.js
+++ index.js
@@ -1,52 +1,80 @@
+// get cost
+function getCost(product) {
+    const value = document.getElementById(product + '-cost').innerText;
+    return value;
+}
+// calcualtion
 function calculation() {
-    const bestCost = document.getElementById('best-cost').innerText;
-    const memoryCost = document.getElementById('memory-cost').innerText;
-    const storageCost = document.getElementById('storage-cost').innerText;
-    const deliveryCost = document.getElementById('delivery-cost').innerText;
+    const bestCost = getCost('best');
+    const memoryCost = getCost('memory');
+    const storageCost = getCost('storage');
+    const deliveryCost = getCost('delivery');
     const cost = parseInt(bestCost) + parseInt(memoryCost) + parseInt(storageCost) + parseInt(deliveryCost);
     document.getElementById('total-cost').innerText = cost;
     document.getElementById('all-total').innerText = cost;
 }
-
+// extra memory cost
+function extraMemoryCost(value) {
+    const extraMemory = document.getElementById('memory-cost');
+    if (value == true) {
+        extraMemory.innerText = 0;
+    }
+    else {
+        extraMemory.innerText = 180;
+    }
+    calculation();
+}
+// extra storage cost
+function extraStorageCost(value) {
+    const extraStorage = document.getElementById('storage-cost');
+    if (value == 0) {
+        extraStorage.innerText = 0;
+    }
+    else if (value == 100) {
+        extraStorage.innerText = 100;
+    }
+    else {
+        extraStorage.innerText = 180;
+    }
+    calculation();
+}
+// delivery charge
+function extraDeliveryCost(value) {
+    const deliveryCost = document.getElementById('delivery-cost');
+    if (value == true) {
+        deliveryCost.innerText = 0;
+    }
+    else {
+        deliveryCost.innerText = 20;
+    }
+    calculation();
+}
 // memory
 document.getElementById('8gb-btn').addEventListener('click', function () {
-    const extraMemory = document.getElementById('memory-cost');
-    extraMemory.innerText = 0;
-    calculation();
+    extraMemoryCost(true);
 });
 document.getElementById('16gb-btn').addEventListener('click', function () {
-    const extraMemory = document.getElementById('memory-cost');
-    extraMemory.innerText = 180;
-    calculation();
+    extraMemoryCost(false);
 });
 
 // storage
 document.getElementById('256gb-btn').addEventListener('click', function () {
-    const extraMemory = document.getElementById('storage-cost');
-    extraMemory.innerText = 0;
-    calculation();
+    extraStorageCost(0);
 });
 document.getElementById('512gb-btn').addEventListener('click', function () {
-    const extraMemory = document.getElementById('storage-cost');
-    extraMemory.innerText = 100;
-    calculation();
+    extraStorageCost(100);
 });
 document.getElementById('1tb-btn').addEventListener('click', function () {
-    const extraMemory = document.getElementById('storage-cost');
-    extraMemory.innerText = 180;
-    calculation();
+    extraStorageCost(180);
 });
 // delivery charge
 document.getElementById('free-btn').addEventListener('click', function () {
-    const extraMemory = document.getElementById('delivery-cost');
-    extraMemory.innerText = 0;
-    calculation();
+    extraDeliveryCost(true);
 });
 document.getElementById('charge-btn').addEventListener('click', function () {
-    const extraMemory = document.getElementById('delivery-cost');
-    extraMemory.innerText = 20;
-    calculation();
+    extraDeliveryCost(false);
 });
+// discount
 document.getElementById('apply-btn').addEventListener('click', function () {
     const applyInput = document.getElementById('apply-input').value;
     if (applyInput.toLowerCase() == 'stevekaku'.toLowerCase()) {
