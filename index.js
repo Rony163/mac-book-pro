4352de8db6827918dfef9f1b856915ce7384d49d
Rony163 <minhajulislamrony43@gmail.com>
2021-08-21 11:14:54 +0600

    another part

--- index.js
+++ index.js
@@ -1,60 +1,58 @@
-
+function calculation() {
+    const bestCost = document.getElementById('best-cost').innerText;
+    const memoryCost = document.getElementById('memory-cost').innerText;
+    const storageCost = document.getElementById('storage-cost').innerText;
+    const deliveryCost = document.getElementById('delivery-cost').innerText;
+    const cost = parseInt(bestCost) + parseInt(memoryCost) + parseInt(storageCost) + parseInt(deliveryCost);
+    document.getElementById('total-cost').innerText = cost;
+    document.getElementById('all-total').innerText = cost;
+}
 // memory
 document.getElementById('8gb-btn').addEventListener('click', function () {
-    const extraMemory = document.getElementById('extra-memory');
+    const extraMemory = document.getElementById('memory-cost');
     extraMemory.innerText = 0;
-    const priceFirst = document.getElementById('total-price');
-    const curentPrice = priceFirst.innerText;
-    const totalPrice = parseInt(curentPrice) - parseInt(180);
-    priceFirst.innerText = totalPrice;
-
+    calculation();
 });
 document.getElementById('16gb-btn').addEventListener('click', function () {
-    const extraMemory = document.getElementById('extra-memory');
+    const extraMemory = document.getElementById('memory-cost');
     extraMemory.innerText = 180;
-    const priceFirst = document.getElementById('total-price');
-    const curentPrice = priceFirst.innerText;
-    const totalPrice = parseInt(curentPrice) + parseInt(extraMemory.innerText);
-    priceFirst.innerText = totalPrice;
+    calculation();
 });
 
 // storage
 document.getElementById('256gb-btn').addEventListener('click', function () {
-    const extraMemory = document.getElementById('extra-storage');
+    const extraMemory = document.getElementById('storage-cost');
     extraMemory.innerText = 0;
-    const priceFirst = document.getElementById('total-price');
-    const curentPrice = priceFirst.innerText;
+    calculation();
 });
 document.getElementById('512gb-btn').addEventListener('click', function () {
-    const extraMemory = document.getElementById('extra-storage');
+    const extraMemory = document.getElementById('storage-cost');
     extraMemory.innerText = 100;
-    const priceFirst = document.getElementById('total-price');
-    const curentPrice = priceFirst.innerText;
-    const totalPrice = parseInt(curentPrice) + parseInt(extraMemory.innerText);
-    priceFirst.innerText = totalPrice;
+    calculation();
 });
 document.getElementById('1tb-btn').addEventListener('click', function () {
-    const extraMemory = document.getElementById('extra-storage');
+    const extraMemory = document.getElementById('storage-cost');
     extraMemory.innerText = 180;
-    const priceFirst = document.getElementById('total-price');
-    const curentPrice = priceFirst.innerText;
-    const totalPrice = parseInt(curentPrice) + parseInt(extraMemory.innerText);
-    priceFirst.innerText = totalPrice;
+    calculation();
 });
 // delivery charge
 document.getElementById('free-btn').addEventListener('click', function () {
-    const extraMemory = document.getElementById('extra-cost');
+    const extraMemory = document.getElementById('delivery-cost');
     extraMemory.innerText = 0;
-    const priceFirst = document.getElementById('total-price');
-    const curentPrice = priceFirst.innerText;
-    const totalPrice = parseInt(curentPrice) - parseInt(20);
-    priceFirst.innerText = totalPrice;
+    calculation();
 });
 document.getElementById('charge-btn').addEventListener('click', function () {
-    const extraMemory = document.getElementById('extra-cost');
+    const extraMemory = document.getElementById('delivery-cost');
     extraMemory.innerText = 20;
-    const priceFirst = document.getElementById('total-price');
-    const curentPrice = priceFirst.innerText;
-    const totalPrice = parseInt(curentPrice) + parseInt(extraMemory.innerText);
-    priceFirst.innerText = totalPrice;
+    calculation();
+});
+document.getElementById('apply-btn').addEventListener('click', function () {
+    const applyInput = document.getElementById('apply-input').value;
+    if (applyInput.toLowerCase() == 'stevekaku'.toLowerCase()) {
+        document.getElementById('apply-input').value = '';
+        const finalTotal = parseInt(document.getElementById('all-total').innerText);
+        document.getElementById('all-total').innerText = finalTotal - (finalTotal * 20 / 100);
+    } else {
+        document.getElementById('apply-input').value = '';
+    }
 });
